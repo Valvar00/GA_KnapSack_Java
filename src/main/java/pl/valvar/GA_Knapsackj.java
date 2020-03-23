f5afe3ddc992c4459b00456715240c47afc94dbc
Artem Shchur <artem.shchur@gmail.com>
2020-03-23 15:32:20 +0200

    Multiple Graph included

--- src/main/java/pl/valvar/GA_Knapsackj.java
+++ src/main/java/pl/valvar/GA_Knapsackj.java
@@ -1,20 +1,26 @@
 package pl.valvar;
 
+import org.knowm.xchart.*;
+import org.knowm.xchart.internal.chartpart.Chart;
+import org.knowm.xchart.style.Styler;
+import org.knowm.xchart.style.markers.SeriesMarkers;
+
 import java.io.*;
 import java.util.Random;
 
 public class GA_Knapsackj {
     public static int[][] csv_data = new int[1501][3];
     private static Individual bIndividual = new Individual();
-    private static Individual sbIndividual = new Individual();
     public static int num_obj = 1500;
     public static int max_cap = 15000;
     public static int max_size = 20000;
     public static String nameofCSV = "kcp.csv";
-    public static int pop_size = 1000;
-    public static double crossover_rate = 0.35;
+    public static int pop_size = 2000;
+    public static double crossover_rate = 0.25;
     public static double mutation_rate = 0.015;
     public static int check = 0;
+    public static int gen_number = 100;
+    public static int tournament_size = 300;
 
     public static void main(String[] args) throws IOException {
         //Generator();
@@ -25,30 +31,35 @@ public class GA_Knapsackj {
             population[i].init();
 
         }
-        for (int i = 0; i < 100; i++) {
-            Random rand = new Random();
-            Individual[] newPopulation = new Individual[pop_size];
-            for (int j = 2; j < pop_size; j++) {
-                int i1 = rand.nextInt(pop_size);
-                newPopulation[0] = getLargest(population);
-                newPopulation[1] = tournament(population,100);
-                newPopulation[j] = newPopulation[0].crossover_operator(newPopulation[1]);
-                newPopulation[j].mutate();
-                newPopulation[j].evaluate();
+        // Create Chart
+        XYChart chart = new XYChartBuilder().width(960).height(540).title("Crossover").xAxisTitle("Generations").yAxisTitle("Score").build();
+        for (int k = 1; k < 6; k++) {
+            double[] pop_i = new double[gen_number];
+            double[] score_i = new double[gen_number];
+            for (int i = 0; i < gen_number; i++) {
+                Individual[] newPopulation = new Individual[pop_size];
+                for (int j = 2; j < pop_size; j++) {
+                    newPopulation[0] = getLargest(population);
+                    newPopulation[1] = tournament(population, tournament_size);
+                    newPopulation[j] = newPopulation[0].crossover_operator(newPopulation[1]);
+                    newPopulation[j].mutate();
+                    newPopulation[j].evaluate();
+                }
+                population = newPopulation;
+//            int best_buffer_old = bIndividual.score;
+//            int best_buffer_new = getLargest(population).score;
+//            if (best_buffer_new!=best_buffer_old){
+//                System.out.println("Population " + i);
+//                System.out.println("Best score:" + getLargest(population).score);
+//                System.out.println();
+//            }
+                pop_i[i] = (double) i;
+                score_i[i] = (double) getLargest(population).score;
             }
-            population = newPopulation;
-            int best_buffer_old = bIndividual.score;
-            int best_buffer_new = getLargest(population).score;
-            if (best_buffer_new!=best_buffer_old){
-                System.out.println("Population " + i);
-                System.out.println("Best score:" + bIndividual.score);
-                System.out.println();
-            }
+            chart.addSeries(k+"st", pop_i, score_i);
+            System.out.println("Best score : " + getLargest(population).score);
         }
-        System.out.println("Best score : " + getLargest(population).score);
-//        for (int j = 0; j < num_obj; j++) {
-//            System.out.print(getLargest(population).chromoset[j]);
-//        }
+        new SwingWrapper<XYChart>(chart).displayChart();
     }
 
     private static int[] RandomGenerator(){
@@ -130,7 +141,6 @@ public class GA_Knapsackj {
         for(int i=1;i<pop_size;i++){
             if(best_score<=pop[i].score){
                 best_score = pop[i].score;
-                sbIndividual = bIndividual;
                 bIndividual=pop[i];
             }
         }
